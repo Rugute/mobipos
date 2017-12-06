19efa53ee39a1cf3b4c51f4050a208954a569613
royWati <royywati@gmail.com>
2017-12-06 14:13:07 +0300

    master

--- app/src/main/java/com/mobipos/app/Defaults/SplashPage.java
+++ app/src/main/java/com/mobipos/app/Defaults/SplashPage.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,21 +16,24 @@ import com.mobipos.app.R;
  */
 
 public class SplashPage extends Activity {
-
+    public String[] homepage_headings={"ADMIN","CASHIER"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_page);
 
         ImageView imageView= findViewById(R.id.image_view);
+        GridView gridView= findViewById(R.id.grid_view);
         Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_fade_in);
         Animation animation_text= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_names);
         Animation animation_linear= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.text_translation);
         imageView.startAnimation(animation);
         TextView textView= findViewById(R.id.splash_text);
-        LinearLayout linear=findViewById(R.id.linear);
+        LinearLayout linear=findViewById(R.id.linearItems);
         textView.setAnimation(animation_text);
         linear.setAnimation(animation_linear);
 
+
+       // gridView.setAdapter(new HomepageAdapter(homepage_headings));
     }
 }
