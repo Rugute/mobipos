0b4a352e88eaa00df79b86ef713850026b72a063
royWati <royywati@gmail.com>
2018-03-10 17:23:46 +0300

    master

---
+++ app/src/main/java/com/mobipos/app/Defaults/PrinterData.java
@@ -0,0 +1,21 @@
+package com.mobipos.app.Defaults;
+
+/**
+ * Created by root on 3/10/18.
+ */
+
+public class PrinterData {
+
+        public String printer_id,printer_name,printer_mac,branch;
+
+        public PrinterData(String printer_id, String printer_name, String printer_mac,String branch) {
+            this.printer_id=printer_id;
+            this.printer_name=printer_name;
+            this.printer_mac=printer_mac;
+            this.branch=branch;
+
+
+        }
+    }
+
+
